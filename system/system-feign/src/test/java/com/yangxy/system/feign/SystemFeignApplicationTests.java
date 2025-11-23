7ba87553207c4b365f7870c1945e82b1df66310a
MotorYang <motoyangxy@outlook.com>
2025-11-23 18:52:59 +0800

    init project

---
+++ system/system-feign/src/test/java/com/yangxy/system/feign/SystemFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangxy.system.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ system/system-main/src/main/java/com/yangxy/system/main/SystemMainApplication.java
@@ -0,0 +1,13 @@
+package com.yangxy.system.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemMainApplication.class, args);
+    }
+
+}
