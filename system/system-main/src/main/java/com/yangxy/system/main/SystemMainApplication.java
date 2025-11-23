a943b16ecbb165764ef66408399545277e80a0df
MotorYang <motoyangxy@outlook.com>
2025-11-24 05:16:38 +0800

    新增多环境配置支持

--- system/system-main/src/main/java/com/yangxy/system/main/SystemMainApplication.java
+++ system/system-main/src/main/java/com/yangxy/system/main/SystemMainApplication.java
@@ -1,13 +1,24 @@
 package com.yangxy.system.main;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class SystemMainApplication {
+public class SystemMainApplication implements CommandLineRunner {
+
+    @Value("${test.value}")
+    private String testValue;
 
     public static void main(String[] args) {
         SpringApplication.run(SystemMainApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("test.value: " + testValue);
+    }
 }
